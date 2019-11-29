350e4faad25c55cf6b1f9fab8c206b4d958641c4
c4181 <cm4181@gmail.com>
2019-11-29 15:11:32 -0500

    fwrite version

---
+++ fwrite/main.cpp
@@ -0,0 +1,50 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::islower;
+using std::tolower;
+using std::toupper;
+
+int main (int argc, char *argv[]) {
+    size_t result;
+    char *fbuffer;
+    long lSize;
+
+    FILE *input_file = fopen(argv[1], "r");
+    FILE *output_file = fopen("fwrite.txt", "w");
+    if (input_file && output_file != nullptr) {
+        // Get file size
+        fseek(input_file, 0, SEEK_END);
+        lSize = ftell(input_file);
+        rewind(input_file);
+
+        // Allocate memory for the entire file
+        fbuffer = (char*)malloc(lSize + 1);
+
+        // Read into the buffer
+        cout << "Read File" << endl;
+        result = fread(fbuffer, sizeof(char), lSize, input_file);
+        if (result != lSize) {
+            cout << "Error reading file" << endl;
+        }
+        cout << "File Read" << endl;
+    }
+    
+    for (size_t i = 0; i < lSize; ++i) {
+        if (islower(fbuffer[i])) {
+            fbuffer[i] = toupper(fbuffer[i]);
+        } else {
+            fbuffer[i] = tolower(fbuffer[i]);
+        }
+    }
+
+    cout << "Write File" << endl;
+    fwrite(fbuffer, sizeof(char), lSize, output_file);
+
+    fclose(input_file);
+    fclose(output_file);
+    free(fbuffer);
+}
